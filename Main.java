098d143833cf792fa85d2fea8f1e8e29949ecf4f
Oksana Bodnarchuk <110978467+OksanaBodnarchuk@users.noreply.github.com>
2022-09-11 19:37:02 +0200

    Домашнее задание за 07.09.22

--- Main.java
+++ Main.java
@@ -1,35 +1,40 @@
 public class Main {
     public static void main(String[] args) {
+        /*Написать программу, которая выводит на экран таблицу умножения (таблицу Пифагора). Естественно, использовать циклы.*/
 
-        //С использованием цикла while написать метод, печатающий куб числа от 0 до данного чисда n.
-        //Пример: на входе n=3  на выходе 0,1,8,27
 
-        int num = 3;
-        int num1 = 0;
-        //------------------
-
-        while (num1 <= num) {
-            System.out.printf((num1 * num1 * num1) + " ");
-            num1 += 1;
+        for (int i = 1; i <= 9; i += 1) {
+            for (int n = 1; n <= 9; n += 1) {
+                System.out.printf("%2d ", i * n);
+                if (n == 9) {
+                    System.out.println();
+                }
+            }
         }
-
-        System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("--------------------");
 
 
-        //С использованием цикла while написать метод, печатающий результат  умножения данного числа n  на все целые числа от 0 до n..
-        //Пример: на входе n=3  на выходе 0,3,6,9
+    /*Дана строка и 2 числа int: startIndex, endIndex. Необходимо получить и вывести на экран строку, которая состоит из всех символов исходной строки, начиная с позиции startIndex (включительно) до позиции endIndex (не включительно). Если startIndex  «не попадает в строку» или больше endIndex выводим “error”. Если endIndex не «попадает в строку»,  выводим, начиная со startIndex до конца строки.  Ограничение: в данной задаче нельзя пользоваться стандартными методам String кроме charAt() и length().
+        “Java is a very popular language”,  startIndex=5  endIndex=13  -> “is a ver”
+        “Java is a very popular language”,  startIndex=5  endIndex=-13  -> error
+        “Java is a very popular language”,  startIndex=5  endIndex=136  -> “is a very popular language”*/
+        String str = "Java is a very popular language";
+        int startIndex = 5;
+        int endIndex = 13;
+        String newStr = "";
+        
+        //------------------------
+        System.out.println(str);
 
-        int num2 = 3;
-        int num3 = 0;
-
-        //--------------------
-
-        while (num3 <= num2) {
-            System.out.printf("%d * %d = %d%n", num2, num3, num2 * num3);
-            num3 += 1;
+        if (startIndex >= str.length() || startIndex < 0 || startIndex > endIndex) {
+            newStr = "error";
+        } else if (startIndex == endIndex) {
+            newStr += str.charAt(startIndex);
+        } else {
+            for (; startIndex < endIndex && startIndex < str.length(); startIndex += 1) {
+                newStr += str.charAt(startIndex);
+            }
         }
-
-
+        System.out.println(newStr);
     }
 }
