ef4604443d6bd785e931137d56aa4e68bd94f5b8
Oksana Bodnarchuk <110978467+OksanaBodnarchuk@users.noreply.github.com>
2022-09-13 21:45:01 +0200

    Домашнее задание за 12.09.22

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        /* Написать метод String replace(String str, char oldChar, char newChar) который в переданной строке str
-        меняет все символы  oldChar на символы newChar.  Например: replace(«саша», ‘c’, ‘м’) -> «маша»
-         */
-        char oldChar = 'с';
-        char newChar = 'м';
-        String Str = new String("саша");
+    /* Написать метод String replace(String str, char oldChar, char newChar) который в переданной строке str меняет все символы  oldChar на символы newChar.  Например: replace(«саша», ‘c’, ‘м’) -> «маша» */
+
+    public static void word(char oldChar, char newChar, String Str) {
+
         System.out.print("Old string: " + Str);
         System.out.println();
         String newString = Str.replace(oldChar, newChar);
         System.out.print("New string: " + newString);
+    }
+
+    public static void main(String[] args) {
+        word('с', 'м', new String("саша"));
+        word('а', 'о', new String("саша"));
     }
 }
