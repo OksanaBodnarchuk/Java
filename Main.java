e25ca33014e3a725dfb9184d60b83d6603fdb781
Oksana Bodnarchuk <110978467+OksanaBodnarchuk@users.noreply.github.com>
2022-09-21 01:10:10 +0200

    Домашнее задание за 19.09.22

--- Main.java
+++ Main.java
@@ -1,31 +1,19 @@
 public class Main {
-    /* C помощью цикла for написать метод, печатающий цифры от 1 до  числа  n
-     */
-    public static int num(int i, int n) {
-        for (; i <= n; i += 1) {
-            System.out.printf(i + " ");
-        }
-        return i;
-    }
-    //-----------------------------------------
-
-    /* C помощью цикла for написать метод возвращающий сумму цифр от 1 до числа n
-     */
-    public static int countSum(int begin, int end) {
-        int sum = 0;
-        for (; begin <= end; begin += 1) {
-            sum += begin++;
-        }
-        return sum;
-    }
-
-    //-----------------------------------------
     public static void main(String[] args) {
-        num(1, 10);
-        System.out.println();
 
-        //-----------------------------------------
+        /*Дан массив int. Реализовать метод, который считает сумму положительных  элементов массива.{3, 2, -1, 5, -3} ->10*/
 
-        System.out.printf("Сумма = %d %n", countSum(1, 10));
+        int[] arr = {3, 2, -1, 5, -3};
+        sum(arr);
+    }
+
+    public static void sum(int[] arr) {
+        int arrSum = 0;
+        for (int i = 0; i < arr.length; i+=1) {
+            if (arr[i] > 0) {
+                arrSum += arr[i];
+            }
+        }
+        System.out.println(arrSum);
     }
 }
